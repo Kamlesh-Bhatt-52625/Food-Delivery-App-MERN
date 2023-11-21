b95d89e4dbb7d15d8ef5e875af53b4702f30986d
Kamlesh Bhatt <kb55262555@gmail.com>
2023-11-21 18:15:48 +0530

    Connected to database

--- mernapp/backend/db.js
+++ mernapp/backend/db.js
@@ -2,9 +2,7 @@ const mongoose = require("mongoose");
 const mongoURL =
   "mongodb+srv://quickeats:quickeats@cluster0.o8r5zi1.mongodb.net/?retryWrites=true&w=majority";
 const connect = () => {
-  mongoose.connect(mongoURL, () => {
-    console.log("Database Connection done");
-  });
+  mongoose.connect(mongoURL);
 };
 
 module.exports = connect;
