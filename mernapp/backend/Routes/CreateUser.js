341abf311bb2836c9180972435d6113ea086e7fc
Kamlesh Bhatt <kb55262555@gmail.com>
2023-11-23 16:41:32 +0530

    Signup Started

--- mernapp/backend/Routes/CreateUser.js
+++ mernapp/backend/Routes/CreateUser.js
@@ -1,22 +1,42 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
+const { body, validationResult } = require("express-validator");
 
-router.post("/createuser", async (req, res) => {
-  try {
-    const { name, password, email, location } = req.body;
-    await User.create({
-      name: name,
-      password: password,
-      email: email,
-      location: location,
-    });
+router.post(
+  "/createuser",
+  [
+    // username must be an email
+    body("email").isEmail(),
+    //
+    body("name").isLength({ min: 5 }),
+    // password must be at least 5 chars long
+    body("password", "Password must be alteast 5 characters.").isLength({
+      min: 5,
+    }),
+  ],
+  async (req, res) => {
+    // Finds the validation errors in this request and wraps them in an object with handy functions
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-    res.json({ success: true });
-  } catch (error) {
-    console.log(error);
-    res.json({ success: false });
+    try {
+      const { name, password, email, location } = req.body;
+      await User.create({
+        name: name,
+        password: password,
+        email: email,
+        location: location,
+      });
+
+      res.json({ success: true });
+    } catch (error) {
+      console.log(error);
+      res.json({ success: false });
+    }
   }
-});
+);
 
 module.exports = router;
--- mernapp/backend/index.js
+++ mernapp/backend/index.js
@@ -2,13 +2,20 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const connect = require("./db");
-const User = require("./models/User");
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-
-app.use(express.json());
 
 app.use("/api", require("./Routes/CreateUser"));
 
