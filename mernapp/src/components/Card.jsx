0684839662356a9d9d3fccd0eef860b9136dc713
Kamlesh Bhatt <kb55262555@gmail.com>
2023-11-25 18:37:30 +0530

    Image source added

--- mernapp/src/components/Card.jsx
+++ mernapp/src/components/Card.jsx
@@ -12,11 +12,7 @@ const Card = (props) => {
         <div
           className="card mt-3"
           style={{ width: "18rem", maxHeight: "360px" }}>
-          <img
-            src="https://img.freepik.com/free-photo/big-sandwich-hamburger-with-juicy-beef-burger-cheese-tomato-red-onion-wooden-table_2829-19631.jpg"
-            className="card-img-top"
-            alt="..."
-          />
+          <img src={image} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{foodName}</h5>
             <div className="container wd-100">
