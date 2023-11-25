b1cd23e3e26d4a31e19d2bcd5020cc55be692a03
Kamlesh Bhatt <kb55262555@gmail.com>
2023-11-25 17:37:59 +0530

    Data aquired for frontend

--- mernapp/src/components/Footer.jsx
+++ mernapp/src/components/Footer.jsx
@@ -4,19 +4,19 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <div>
-      <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-        <div class="col-md-4 d-flex align-items-center">
+      <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+        <div className="col-md-4 d-flex align-items-center">
           <Link
             to="/"
-            class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1"></Link>
-          <span class="text-muted">© 2023 QuickEats, Inc</span>
+            className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1"></Link>
+          <span className="text-muted">© 2023 QuickEats, Inc</span>
         </div>
 
         {/* SocialMedia Section*/}
-        <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
-          <li class="ms-3">
-            <Link class="text-muted" to="/">
-              <svg class="bi" width="24" height="24">
+        <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
+          <li className="ms-3">
+            <Link className="text-muted" to="/">
+              <svg className="bi" width="24" height="24">
                 {/* Logo for SocicalMedia */}
               </svg>
             </Link>
--- mernapp/src/screens/Home.jsx
+++ mernapp/src/screens/Home.jsx
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Card from "../components/Card";
 import Carousel from "../components/Carousel";
 
 const Home = () => {
+  // BACKEND URL
+  const backendURL = `http://localhost:8080`;
 
-  
+  const [foodCategory, setFoodCategory] = useState([]);
+  const [foodItem, setFoodItem] = useState([]);
 
+  // To fetch the foodItems and foodCategory from the backend
+  const loadData = async () => {
+    let res = await fetch(`${backendURL}/api/foodData`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    res = await res.json();
+    console.log(res[0], res[1]);
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
 
   return (
     <div>
