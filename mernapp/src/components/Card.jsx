c82e3d7d651f5545d14b49420ff2a298e5039d51
Kamlesh Bhatt <kb55262555@gmail.com>
2023-11-26 13:30:44 +0530

    Few other stuffs added to cart

--- mernapp/src/components/Card.jsx
+++ mernapp/src/components/Card.jsx
@@ -14,6 +14,36 @@ const Card = (props) => {
   const [size, setSize] = useState("");
 
   const handleAddToCart = async () => {
+    let food = [];
+    for (const item of data) {
+      if (item.id === props.foodItem._id) {
+        food = item;
+        break;
+      }
+    }
+
+    if (food) {
+      if (food.size === size) {
+        await dispatch({
+          type: "UPDATE",
+          id: props.foodItem._id,
+          price: finalPrice,
+          qty: qty,
+        });
+        return;
+      } else if (food.size !== size) {
+        await dispatch({
+          type: "ADD",
+          id: props.foodItem._id,
+          name: props.foodItem.name,
+          price: finalPrice,
+          qty: qty,
+          size: size,
+        });
+        return;
+      }
+      return;
+    }
     await dispatch({
       type: "ADD",
       id: props.foodItem._id,
@@ -22,7 +52,6 @@ const Card = (props) => {
       qty: qty,
       size: size,
     });
-    console.log(data);
   };
 
   let finalPrice = qty * parseInt(options[size]);
