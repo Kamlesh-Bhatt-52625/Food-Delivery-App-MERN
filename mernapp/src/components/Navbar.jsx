41e8ca0cf92c60309ceed98d425ef111f6e3ba51
Kamlesh Bhatt <kb55262555@gmail.com>
2023-11-26 13:48:17 +0530

    Few other stuffs added to cart

--- mernapp/src/components/Navbar.jsx
+++ mernapp/src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   const navigate = useNavigate();
   const handleLogout = () => {
     localStorage.removeItem("authToken");
-    navigate("/login");
+    navigate("/");
   };
 
   return (
